aa0c57b88b5f80c73c9c5609e9ec5278deb4aa94
muh-arifulislam <arifibnenam@gmail.com>
2024-07-03 23:36:48 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  }
+);
